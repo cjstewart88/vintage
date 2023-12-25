e639ef1dde1c05c650e51fdccf5313c7068fdc59
Cody Stewart <cjstewart88@users.noreply.github.com>
2023-12-24 21:58:55 -0500

    search all puzzle games

--- index.js
+++ index.js
@@ -4,26 +4,29 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
 
-const url = "https://apps.apple.com/us/genre/ios-games-puzzle/id7012";
+const characters = 'ABCDEFGHIJKLMNOPQRSTUV*'.split('');
 
-async function run() {
-  const games = [];
+const games = [];
+const concurrencyLimit = 5;
 
-  const response = await axios.get(url);
-  const $ = cheerio.load(response.data);
+const fetchPage = async (character, pageNum) => {
+  try {
+    const pageResponse = await axios.get(`https://apps.apple.com/us/genre/ios-games-puzzle/id7012?letter=${character}&page=${pageNum}`);
+    const page = cheerio.load(pageResponse.data);
+    const gameLinks = page("#selectedcontent ul li a");
 
-  const gameElements = $("#selectedcontent ul li a");
-  // const firstTwo = gameElements.slice(0, 2);
+    // doing this because "L"s have a game that shows up on every page.
+    // ie: page 1000 will have this one game on it
+    if (gameLinks.length <= 1) {
+      return null;
+    } else {
+      const requests = gameLinks.slice(0, concurrencyLimit).map(async (i, el) => {
+        const game = {
+          name: el.children[0].data,
+          url: el.attribs.href,
+        };
 
-  const requests = gameElements.map(async (i, el) => {
-    const game = {
-      name: el.children[0].data,
-      url: el.attribs.href,
-    };
-
-    await axios
-      .get(game.url)
-      .then((gameResponse) => {
+        const gameResponse = await axios.get(game.url);
         const gamePage = cheerio.load(gameResponse.data);
 
         const price = gamePage(".app-header__list__item--price")
@@ -54,15 +57,30 @@ async function run() {
           game.price === "free" &&
           game.ipadOSVersion <= 9
         ) {
+          console.log(game)
           games.push(game);
         }
-      })
-      .catch((error) => {
-        console.log(`Error fetching ${game.url}: ${error.message}`);
       });
-  });
 
-  await Promise.all(requests);
+      await Promise.all(requests);
+
+      return pageNum + 1;
+    }
+  } catch (error) {
+    console.log(`Error fetching ${character} - Page ${pageNum}: ${error.message}`);
+  }
+};
+
+const run = async () => {
+  for (const character of characters) {
+    let page = 1;
+    while (page) {
+      const pagesToFetch = Array.from({ length: concurrencyLimit }, (_, i) => page + i);
+      const results = await Promise.all(pagesToFetch.map(pageNum => fetchPage(character, pageNum)));
+      page = results.find(result => result !== null);
+    }
+  }
+
   fs.writeFile("results.json", JSON.stringify(games, null, 2), (err) => {
     if (err) {
       console.error("Error writing to the file:", err);
@@ -70,7 +88,6 @@ async function run() {
       console.log("Results written to results.json");
     }
   });
-}
+};
 
-// Run the scraping function
 run();
